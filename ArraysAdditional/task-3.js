5984a1fe81a160d0e2a59cd6b635466ef739993a
TitovTrio <titovtrio@gmail.com>
2024-08-12 01:42:16 +0300

    Add files via upload

---
+++ ArraysAdditional/task-3.js
@@ -0,0 +1,8 @@
+let matrix = [];
+for (let i = 0; i < 3; i++) {
+    let array = [];
+    for (let j = 1; j <= 5; j++) {
+        array.push(j);
+    }
+    matrix.push(array);
+}
---
+++ ArraysAdditional/task-4.js
@@ -0,0 +1,11 @@
+const matrix = [
+    [ 1, 2, 3 ],
+    [ 4, 5, 6 ],
+    [ 7, 8, 9 ],
+ ];
+
+ const result = [];
+ for (let i = 0; i < matrix.length; i++){
+    result.push(...matrix[i])
+ }
+ console.log(result)
